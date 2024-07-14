b2fcae5eda3ae502a3e5655e634fed99055257b4
erica243 <erica204chavez@gmail.com>
2024-07-14 17:55:41 +0800

    commit

--- database/fos_db.sql
+++ database/fos_db.sql
@@ -11,7 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -28,11 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cart` (
-  `id` int(30) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `client_ip` varchar(20) NOT NULL,
-  `user_id` int(30) NOT NULL,
-  `product_id` int(30) NOT NULL,
-  `qty` int(30) NOT NULL
+  `user_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `qty` int NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -52,8 +52,9 @@ INSERT INTO `cart` (`id`, `client_ip`, `user_id`, `product_id`, `qty`) VALUES
 --
 
 CREATE TABLE `category_list` (
-  `id` int(30) NOT NULL,
-  `name` text NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -72,35 +73,36 @@ INSERT INTO `category_list` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `orders` (
-  `id` int(30) NOT NULL,
- `order_number` text NOT NULL,
- `order_date` text NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_number` text NOT NULL,
+  `order_date` text NOT NULL,
   `name` text NOT NULL,
   `address` text NOT NULL,
   `mobile` text NOT NULL,
   `email` text NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `delivery_method` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `name`, `address`, `mobile`, `email`, `status`, `created_at`) VALUES
-(1, 'James Smith', 'adasdasd asdadasd', '4756463215', 'jsmith@sample.com', 1, '2024-07-03 12:24:05'),
-(2, 'James Smith', 'adasdasd asdadasd', '4756463215', 'jsmith@sample.com', 1, '2024-07-03 15:28:37'),
-(3, 'Claire Blake', 'Sample Address', '0912365487', 'cblake@mail.com', 1, '2024-07-04 10:58:53'),
-(4, 'erica adlit', 'tarong', '0915829634', 'manilyndemesa87@gmail.com', 0, '2024-07-03 15:28:37'),
-(5, 'erica adlit', 'tarong', '0915829634', 'manilyndemesa87@gmail.com', 1, '2024-07-03 15:28:37'),
-(6, 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, '2024-07-03 12:40:31'),
-(7, 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, '2024-07-03 15:29:49'),
-(8, 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, '2024-07-03 09:46:18'),
-(9, 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, '2024-07-04 11:35:01'),
-(10, 'erica adlit', 'tarong', '0915829634', 'mdemesa@gmail.com', 0, '2024-07-04 17:55:24'),
-(11, 'erica adlit', 'tarong', '0915829634', 'mdemesa@gmail.com', 0, '2024-07-04 17:57:17'),
-(12, 'erica adlit', 'tarong', '0915829634', 'mdemesa@gmail.com', 0, '2024-07-04 18:05:25');
+INSERT INTO `orders` (`id`, `order_number`, `order_date`, `name`, `address`, `mobile`, `email`, `status`, `delivery_method`, `created_at`) VALUES
+(1, '001', '2024-07-03', 'James Smith', 'adasdasd asdadasd', '4756463215', 'jsmith@sample.com', 1, 'Courier', '2024-07-03 12:24:05'),
+(2, '002', '2024-07-03', 'James Smith', 'adasdasd asdadasd', '4756463215', 'jsmith@sample.com', 1, 'Courier', '2024-07-03 15:28:37'),
+(3, '003', '2024-07-04', 'Claire Blake', 'Sample Address', '0912365487', 'cblake@mail.com', 1, 'Courier', '2024-07-04 10:58:53'),
+(4, '004', '2024-07-03', 'erica adlit', 'tarong', '0915829634', 'manilyndemesa87@gmail.com', 0, 'Courier', '2024-07-03 15:28:37'),
+(5, '005', '2024-07-03', 'erica adlit', 'tarong', '0915829634', 'manilyndemesa87@gmail.com', 1, 'Courier', '2024-07-03 15:28:37'),
+(6, '006', '2024-07-03', 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, 'Courier', '2024-07-03 12:40:31'),
+(7, '007', '2024-07-03', 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, 'Courier', '2024-07-03 15:29:49'),
+(8, '008', '2024-07-03', 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, 'Courier', '2024-07-03 09:46:18'),
+(9, '009', '2024-07-04', 'Keneth Ducay', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 1, 'Courier', '2024-07-04 11:35:01'),
+(10, '010', '2024-07-04', 'erica adlit', 'tarong', '0915829634', 'mdemesa@gmail.com', 0, 'Courier', '2024-07-04 17:55:24'),
+(11, '011', '2024-07-04', 'erica adlit', 'tarong', '0915829634', 'mdemesa@gmail.com', 0, 'Courier', '2024-07-04 17:57:17'),
+(12, '012', '2024-07-04', 'erica adlit', 'tarong', '0915829634', 'mdemesa@gmail.com', 0, 'Courier', '2024-07-04 18:05:25');
 
 -- --------------------------------------------------------
 
@@ -109,10 +111,11 @@ INSERT INTO `orders` (`id`, `name`, `address`, `mobile`, `email`, `status`, `cre
 --
 
 CREATE TABLE `order_list` (
-  `id` int(30) NOT NULL,
-  `order_id` int(30) NOT NULL,
-  `product_id` int(30) NOT NULL,
-  `qty` int(30) NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `qty` int NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -148,61 +151,36 @@ INSERT INTO `order_list` (`id`, `order_id`, `product_id`, `qty`) VALUES
 --
 
 CREATE TABLE `product_list` (
-  `id` int(30) NOT NULL,
-  `category_id` int(30) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `price` float NOT NULL DEFAULT 0,
   `img_path` text NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0= unavailable, 2 Available'
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0= unavailable, 2=Available',
+  `rating` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product_list`
 --
 
-INSERT INTO `product_list` (`id`, `category_id`, `name`, `description`, `price`, `img_path`, `status`) VALUES
-(9, 8, 'manilyn', 'ff', 350, '1720082160_b3.jpg', 0),
-(10, 7, 'Chocolate cake', 'dgdfg', 350, '1720082160_b9.jpg', 1),
-(11, 7, 'Wedding Cake', 'fsdf', 55543, '1720082160_b8.jpg', 1),
-(12, 7, 'nono', 'sssdd', 111, '1720082760_b1.jpg', 1),
-(14, 8, 'sdsd', 'sdsd', 22, '1720083240_b1.jpg', 1),
-(15, 8, 'qq', 'as', 1, '1720083240_b6.jpg', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_ratings`
---
-
-CREATE TABLE `product_ratings` (
-  `id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `rating` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `system_settings`
---
-
-CREATE TABLE `system_settings` (
-  `id` int(30) NOT NULL,
-  `name` text NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `contact` varchar(20) NOT NULL,
-  `cover_img` text NOT NULL,
-  `about_content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `system_settings`
---
-
-INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
-(1, 'M&M Cake Ordering System', 'erica204chavez@gmail.com', '+639158259643', '1720081920_logo1.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;ABOUT US&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;b style=&quot;margin: 0px; padding: 0px; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; text-align: justify;&quot;&gt;Lorem Ipsum&lt;/b&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&amp;#x2019;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&lt;br&gt;&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;h2 style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;Where does it come from?&lt;/h2&gt;&lt;p style=&quot;text-align: center; margin-bottom: 15px; padding: 0px; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400;&quot;&gt;Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32.&lt;/p&gt;&lt;/span&gt;&lt;/b&gt;&lt;/span&gt;&lt;/p&gt;');
+INSERT INTO `product_list` (`id`, `category_id`, `name`, `description`, `price`, `img_path`, `status`, `rating`) VALUES
+(3, 1, 'Choco Mocha', 'Description for Choco Mocha', 600, '', 1, 0),
+(5, 1, 'Choco Mousse Cake', 'Description for Choco Mousse Cake', 600, '', 1, 0),
+(6, 1, 'Mocha Cake', 'Description for Mocha Cake', 650, '', 1, 0),
+(7, 1, 'Ube Cake', 'Description for Ube Cake', 650, '', 1, 0),
+(8, 1, 'Blueberry Cheesecake', 'Description for Blueberry Cheesecake', 800, '', 1, 0),
+(9, 1, 'Red Velvet Cake', 'Description for Red Velvet Cake', 800, '', 1, 0),
+(10, 1, 'Choco Butternut Cake', 'Description for Choco Butternut Cake', 700, '', 1, 0),
+(11, 1, 'Black Forest Cake', 'Description for Black Forest Cake', 700, '', 1, 0),
+(12, 1, 'Carrot Cake', 'Description for Carrot Cake', 700, '', 1, 0),
+(13, 1, 'Mango Cake', 'Description for Mango Cake', 650, '', 1, 0),
+(14, 1, 'Pineapple Cake', 'Description for Pineapple Cake', 650, '', 1, 0),
+(15, 1, 'Sans Rival', 'Description for Sans Rival', 650, '', 1, 0),
+(16, 1, 'Fruit Cake', 'Description for Fruit Cake', 650, '', 1, 0),
+(17, 1, 'Banana Cake', 'Description for Banana Cake', 650, '', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -211,163 +189,30 @@ INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `a
 --
 
 CREATE TABLE `users` (
-  `id` int(30) NOT NULL,
-  `name` varchar(200) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` text NOT NULL,
+  `middlename` text NOT NULL,
+  `lastname` text NOT NULL,
   `username` text NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=admin , 2 = staff'
+  `password` text NOT NULL,
+  `avatar` text NOT NULL,
+  `address` text NOT NULL,
+  `contact` text NOT NULL,
+  `email` text NOT NULL,
+  `type` int NOT NULL DEFAULT 2 COMMENT '1=admin 2=user',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
-(1, 'Administrator', 'admin', '$2y$10$efDvenHYJ5Fu/xxt1ANbXuRx5/TuzNs/s4k6keUiiFvr2ueE0GmrG', 1),
-(2, 'Staff', 'staff', '$2y$10$/.XdnTz470yEg2DbdtxZae5MkRBWUADIo5nE6UWfn49tKWBZSgJLK', 2);
+INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `address`, `contact`, `email`, `type`, `created_at`) VALUES
+(2, 'James', 'D', 'Smith', 'jsmith', 'password', '', '123 Main St', '4756463215', 'jsmith@sample.com', 2, '2024-07-03 15:29:49'),
+(3, 'Claire', '', 'Blake', 'cblake', 'password', '', '456 Elm St', '0912365487', 'cblake@mail.com', 2, '2024-07-03 10:58:53'),
+(4, 'Keneth', '', 'Ducay', 'kenethducay12', 'password', '', 'Atop-Atop, Bantayan, Cebu', '0915829634', 'kenethducay12@gmail.com', 2, '2024-07-03 09:46:18');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_info`
---
-
-CREATE TABLE `user_info` (
-  `user_id` int(10) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `email` varchar(300) NOT NULL,
-  `password` varchar(300) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `address` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `user_info`
---
-
-INSERT INTO `user_info` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address`) VALUES
-(1, 'James', 'Smith', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', '4756463215', 'adasdasd asdadasd'),
-(2, 'Claire', 'Blake', 'cblake@mail.com', '$2y$10$QYX8P9KwBKXunMEE4I5hVO/hO9pxUU/aswTlf.v.Uy1CNDEabTafS', '0912365487', 'Sample Address'),
-(3, 'erica', 'adlit', 'manilyndemesa87@gmail.com', '$2y$10$lye723KpwQVP76Urjas03OBI/I0AUVxkGJaGtNteHZL.9c000gjmK', '0915829634', 'tarong'),
-(4, 'Keneth', 'Ducay', 'kenethducay12@gmail.com', '$2y$10$x9FMoDT/WR2Ungg.8kutnO.o3LtuMq/vimb4uhO8dWnCAY/S2XrUe', '0915829634', 'Atop-Atop, Bantayan, Cebu'),
-(5, 'erica', 'adlit', 'mdemesa@gmail.com', '$2y$10$nde4A5aYOfo8tyj2M4eoOOVnbcavB4nusFunQWDFVhBVIEhhTAznK', '0915829634', 'tarong');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `category_list`
---
-ALTER TABLE `category_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_list`
---
-ALTER TABLE `order_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product_list`
---
-ALTER TABLE `product_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product_ratings`
---
-ALTER TABLE `product_ratings`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `system_settings`
---
-ALTER TABLE `system_settings`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_info`
---
-ALTER TABLE `user_info`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cart`
---
-ALTER TABLE `cart`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
-
---
--- AUTO_INCREMENT for table `category_list`
---
-ALTER TABLE `category_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `order_list`
---
-ALTER TABLE `order_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
--- AUTO_INCREMENT for table `product_list`
---
-ALTER TABLE `product_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `product_ratings`
---
-ALTER TABLE `product_ratings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `system_settings`
---
-ALTER TABLE `system_settings`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `user_info`
---
-ALTER TABLE `user_info`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
