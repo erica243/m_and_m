5de367a6e460c240a3a68662e38d83a7dc07ccc8
erica243 <erica204chavez@gmail.com>
2024-07-14 20:42:13 +0800

    s

--- signup.php
+++ signup.php
@@ -32,28 +32,31 @@
 
 
 <script>
-	$('#signup-frm').submit(function(e){
-		e.preventDefault()
-		$('#signup-frm button[type="submit"]').attr('disabled',true).html('Saving...');
-		if($(this).find('.alert-danger').length > 0 )
-			$(this).find('.alert-danger').remove();
-		$.ajax({
-			url:'admin/ajax.php?action=signup',
-			method:'POST',
-			data:$(this).serialize(),
-			error:err=>{
-				console.log(err)
-		$('#signup-frm button[type="submit"]').removeAttr('disabled').html('Create');
+    $('#signup-frm button[type="button"]').click(function(){
+        $('#signup-frm').submit();
+    });
 
-			},
-			success:function(resp){
-				if(resp == 1){
-					location.href ='<?php echo isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php?page=home' ?>';
-				}else{
-					$('#signup-frm').prepend('<div class="alert alert-danger">Email already exist.</div>')
-					$('#signup-frm button[type="submit"]').removeAttr('disabled').html('Create');
-				}
-			}
-		})
-	})
+    $('#signup-frm').submit(function(e){
+        e.preventDefault();
+        $('#signup-frm button[type="button"]').attr('disabled',true).html('Saving...');
+        if($(this).find('.alert-danger').length > 0 )
+            $(this).find('.alert-danger').remove();
+        $.ajax({
+            url:'admin/ajax.php?action=signup',
+            method:'POST',
+            data:$(this).serialize(),
+            error:err=>{
+                console.log(err);
+                $('#signup-frm button[type="button"]').removeAttr('disabled').html('Create');
+            },
+            success:function(resp){
+                if(resp == 1){
+                    location.href ='<?php echo isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php?page=home' ?>';
+                }else{
+                    $('#signup-frm').prepend('<div class="alert alert-danger">Email already exists.</div>');
+                    $('#signup-frm button[type="button"]').removeAttr('disabled').html('Create');
+                }
+            }
+        });
+    });
 </script>
