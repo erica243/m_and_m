5307aea929ec6b9c7f21c7d3973f87f9aa9ad6f6
erica243 <erica204chavez@gmail.com>
2024-07-22 17:59:41 +0800

    a

--- admin/home.php
+++ admin/home.php
@@ -178,22 +178,20 @@ for ($i = 0; $i < 12; $i++) {
         </div>
 
         <div class="row m-3">
-            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
-                <div class="card rounded-0 shadow card-custom card-custom-primary bg-light-blue">
-                    <div class="card-body">
-                    <div class="card p-30" style="background:#9999ff; color: #39ac73
-    ;">
-                                                <div class="media">
-                                                    <div class="media-left meida media-middle"> 
-                                                        <span><i class="fa fa-bounce fa-money-bill-wave bgreen f-s-40" aria-hidden="true"></i></span>
-                                                    </div>
-                        <div class="container-fluid">
-                            <h5 class="text-muted">Total Sales</h5>
-                            <h2 class="text-right"><b><?= number_format($total_sales, 2) ?></b></h2>
-                        </div>
-                    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
+        <div class="card p-30" style="background:#9999ff; color: #39ac73;">
+            <div class="media">
+                <div class="media-left meida media-middle"> 
+                    <span><i class="fa fa-bounce fa-money-bill-wave bgreen f-s-40" aria-hidden="true"></i></span>
+                </div>
+                <div class="media-body media-text-right">
+                    <h5 class="text-muted">Total Sales</h5>
+                    <h2 class="text-right"><b><?= number_format($total_sales, 2) ?></b></h2>
                 </div>
             </div>
+        </div>
+    </div>
+
 
             <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
                 <div class="card rounded-0 shadow card-custom card-custom-danger bg-light-red">
