6ce4d3af39199b83ad18add015589a53681bd650
erica243 <erica204chavez@gmail.com>
2024-07-23 13:21:44 +0800

    a

--- admin/home.php
+++ admin/home.php
@@ -51,7 +51,24 @@ for ($i = 0; $i < 12; $i++) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
+        @keyframes bounce {
+            0%, 20%, 50%, 80%, 100% {
+                transform: translateY(0);
+            }
+            40% {
+                transform: translateY(-20px);
+            }
+            60% {
+                transform: translateY(-10px);
+            }
+        }
+
+        .bounce {
+            animation: bounce 2s infinite;
+        }
+
         .custom-menu {
             z-index: 1000;
             position: absolute;
@@ -61,6 +78,7 @@ for ($i = 0; $i < 12; $i++) {
             padding: 8px;
             min-width: 13vw;
         }
+
         a.custom-menu-list {
             width: 100%;
             display: flex;
@@ -69,25 +87,31 @@ for ($i = 0; $i < 12; $i++) {
             font-size: 1em;
             padding: 1px 11px;
         }
+
         span.card-icon {
             position: absolute;
             font-size: 3em;
             bottom: .2em;
             color: #ffffff80;
         }
+
         .file-item {
             cursor: pointer;
         }
+
         a.custom-menu-list:hover, .file-item:hover, .file-item.active {
             background: #80808024;
         }
+
         table th, td {
             /*border-left:1px solid gray;*/
         }
+
         a.custom-menu-list span.icon {
             width: 1em;
             margin-right: 5px;
         }
+
         .candidate {
             margin: auto;
             width: 23vw;
@@ -98,12 +122,14 @@ for ($i = 0; $i < 12; $i++) {
             border: 3px solid #00000008;
             background: #8080801a;
         }
+
         .candidate_name {
             margin: 8px;
             margin-left: 3.4em;
             margin-right: 3em;
             width: 100%;
         }
+
         .img-field {
             display: flex;
             height: 8vh;
@@ -115,12 +141,14 @@ for ($i = 0; $i < 12; $i++) {
             left: -.7em;
             top: -.7em;
         }
+
         .candidate img {
             height: 100%;
             width: 100%;
             margin: auto;
             border-radius: 50%;
         }
+
         .vote-field {
             position: absolute;
             right: 0;
@@ -162,195 +190,143 @@ for ($i = 0; $i < 12; $i++) {
         .bg-light-yellow {
             background-color: #fff3cd;
         }
+
+        .fa-bounce {
+            animation: bounce 2s infinite;
+        }
     </style>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="card mt-3">
+            <div class="col-lg-10">
+                <div class="card mt-25">
                     <div class="card-body">
-                       <h1> Dashboard</h1>
+                        <h1>Dashboard</h1>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="row m-3">
-    <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
-        <div class="card p-0" style="background:#ADD8E6; color: #39ac73;">
-            <div class="media">
-                <div class="media-left meida media-middle"> 
-                    <span><i class="fa fa-bounce fa-money-bill-wave bgreen f-s-40" aria-hidden="true"></i></span>
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
+                <div class="card rounded-0 shadow card-custom bg-blue">
+                    <div class="card-body" style="background: #4d94ff; color: green;">
+                        <div class="media">
+                            <div class="media-left meida media-middle"> 
+                                <span><i class="fa fa-money-bill-wave bounce" style="height: 50px; width: 50px;" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="media-body media-text-center">
+                                <h5 class="text-right" style="color: black; font-size: 30px; font-family: courier-new;">Total Sales</h5>
+                                <h2 class="text-right" style="color: black;"><b><?= number_format($total_sales, 2) ?></b></h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="media-body media-text-right">
-                    <h5 class="text-muted">Total Sales</h5>
-                    <h2 class="text-right" style="color: black;"><b><?= number_format($total_sales, 2) ?></b></h2>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
+                <div class="card rounded-0 shadow card-custom bg-light-red">
+                    <div class="card-body" style="background: #ff99ff; color: orange;">
+                        <div class="media">
+                            <div class="media-left meida media-middle"> 
+                                <span><i class="fa fa-clock bounce" style="height: 50px; width: 50px;" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="media-body media-text-center">
+                                <h5 class="text-right" style="color: black; font-size: 30px; font-family: courier-new;">Pending Orders</h5>
+                                <h2 class="text-right" style="color: black;"><b><?= number_format($cancelled_orders) ?></b></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
+                <div class="card rounded-0 shadow card-custom bg-light-green">
+                    <div class="card-body" style="background: #80ff80; color: green;">
+                        <div class="media">
+                            <div class="media-left meida media-middle"> 
+                                <span><i class="fa fa-check-circle bounce" style="height: 50px; width: 50px;" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="media-body media-text-center">
+                                <h5 class="text-right" style="color: black; font-size: 30px; font-family: courier-new;">Confirmed Orders</h5>
+                                <h2 class="text-right" style="color: black;"><b><?= number_format($confirmed_orders) ?></b></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
+                <div class="card rounded-0 shadow card-custom bg-light-yellow">
+                    <div class="card-body" style="background: #ffff99; color: orange;">
+                        <div class="media">
+                            <div class="media-left meida media-middle"> 
+                                <span><i class="fa fa-chart-bar bounce" style="height: 50px; width: 50px;" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="media-body media-text-center">
+                                <h5 class="text-right" style="color: black; font-size: 30px; font-family: courier-new;">Sales This Month</h5>
+                                <h2 class="text-right" style="color: black;"><b><?= number_format(array_sum($monthly_sales_data)) ?></b></h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
-                <div class="card rounded-0 shadow card-custom card-custom-danger bg-light-red">
-                    <div class="card-body">
-                        <div class="container-fluid">
-                            <h5 class="text-muted">Pending Orders</h5>
-                            <h2 class="text-right"><b><?= number_format($cancelled_orders) ?></b></h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
-                <div class="card rounded-0 shadow card-custom card-custom-success bg-light-green">
-                    <div class="card-body">
-                        <div class="container-fluid">
-                            <h5 class="text-muted">Confirmed Orders</h5>
-                            <h2 class="text-right"><b><?= number_format($confirmed_orders) ?></b></h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
-                <div class="card rounded-0 shadow card-custom card-custom-warning bg-light-yellow">
-                    <div class="card-body">
-                        <div class="container-fluid">
-                            <h5 class="text-muted">Monthly Sales</h5>
-                            <?php 
-                            $current_month = date('Y-m');
-                            $monthly_sales_result = $conn->query("SELECT SUM(p.price * ol.qty) AS monthly_sales 
-                                                                 FROM orders o 
-                                                                 JOIN order_list ol ON o.id = ol.order_id
-                                                                 JOIN product_list p ON ol.product_id = p.id 
-                                                                 WHERE o.status = 1 AND DATE_FORMAT(o.created_at, '%Y-%m') = '$current_month'");
-                            $monthly_sales = $monthly_sales_result->fetch_assoc()['monthly_sales'];
-                            ?>
-                            <h2 class="text-right"><b><?= number_format($monthly_sales, 2) ?></b></h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <!-- Pie Chart -->
         <div class="row">
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-body">
-                        <canvas id="salesChart" height="200"></canvas>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-body">
-                        <canvas id="pieChart" height="200"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row mt-3">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-body">
-                        <canvas id="lineChart" height="100"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="col-md-6">
+        <h3>Sales by Product</h3>
+        <canvas id="salesByProductChart" style="width: 100%; height: 400px;"></canvas>
     </div>
+
+    <!-- Bar Chart -->
+    <div class="col-md-6">
+        <h3>Monthly Sales</h3>
+        <canvas id="monthlySalesChart" style="width: 50%; height: 400px;"></canvas>
+    </div>
+</div>
+
 
     <script>
-    var salesData = <?php echo json_encode($data); ?>;
-    var productNames = salesData.map(item => item.product_name);
-    var totalQuantities = salesData.map(item => item.total_qty);
+        // Pie Chart
+        const salesByProductCtx = document.getElementById('salesByProductChart').getContext('2d');
+        const salesByProductData = <?= json_encode($data) ?>;
+        new Chart(salesByProductCtx, {
+            type: 'pie',
+            data: {
+                labels: salesByProductData.map(d => d.product_name),
+                datasets: [{
+                    data: salesByProductData.map(d => d.total_qty),
+                    backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF']
+                }]
+            }
+        });
 
-    var ctxBar = document.getElementById('salesChart').getContext('2d');
-    var salesChart = new Chart(ctxBar, {
-        type: 'bar',
-        data: {
-            labels: productNames,
-            datasets: [{
-                label: 'Sales by Product',
-                data: totalQuantities,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        stepSize: 1 // Adjust step size as needed
+        // Bar Chart
+        const monthlySalesCtx = document.getElementById('monthlySalesChart').getContext('2d');
+        const monthlySalesLabels = Object.keys(<?= json_encode($monthly_sales_data) ?>);
+        const monthlySalesData = Object.values(<?= json_encode($monthly_sales_data) ?>);
+        new Chart(monthlySalesCtx, {
+            type: 'bar',
+            data: {
+                labels: monthlySalesLabels,
+                datasets: [{
+                    label: 'Sales',
+                    data: monthlySalesData,
+                    backgroundColor: '#007bff'
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
                     }
                 }
             }
-        }
-    });
-
-    var pieChartLabels = productNames.slice(0, 6); // Limit to 6 items for better visualization
-    var pieChartData = totalQuantities.slice(0, 6); // Limit to 6 items for better visualization
-
-    var ctxPie = document.getElementById('pieChart').getContext('2d');
-    var pieChart = new Chart(ctxPie, {
-        type: 'pie',
-        data: {
-            labels: pieChartLabels,
-            datasets: [{
-                data: pieChartData,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.8)',
-                    'rgba(54, 162, 235, 0.8)',
-                    'rgba(255, 206, 86, 0.8)',
-                    'rgba(75, 192, 192, 0.8)',
-                    'rgba(153, 102, 255, 0.8)',
-                    'rgba(255, 159, 64, 0.8)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        }
-    });
-
-    var monthlySalesData = <?php echo json_encode(array_values($monthly_sales_data)); ?>;
-    var months = <?php echo json_encode(array_keys($monthly_sales_data)); ?>;
-
-    var ctxLine = document.getElementById('lineChart').getContext('2d');
-    var lineChart = new Chart(ctxLine, {
-        type: 'line',
-        data: {
-            labels: months,
-            datasets: [{
-                label: 'Monthly Sales',
-                data: monthlySalesData,
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+        });
     </script>
-
-    <?php $conn->close(); ?>
 </body>
 </html>
