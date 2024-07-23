5877d7b9abbd433a374de42586c5d3edf69c4dcb
erica243 <erica204chavez@gmail.com>
2024-07-23 21:56:10 +0800

    z

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -3,7 +3,7 @@
 $servername = '127.0.0.1'; // Typically 'localhost' or '127.0.0.1' for local servers
 $username = 'u510162695_fos_db'; // Your database username
 $password = '1Fos_db_password'; // Your database password
-$dbname = 'u510162695_fos_db'; // Your database name
+$dbname = 'fos_db'; // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
