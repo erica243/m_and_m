53528193cde158f74cb9ac82aa92a6a0613a19d3
erica243 <erica204chavez@gmail.com>
2024-09-05 10:55:15 +0800

    COCMMIT

--- header.php
+++ header.php
@@ -21,7 +21,7 @@ function test_cypher_decrypt($encryption){
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title><?php echo $_SESSION['name'] ?></title>
+        <title>M&M CAKE ORDERING SYSTEM</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
