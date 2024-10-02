7d27d25c5bade1b830c4047f8315926c715e40c7
erica243 <erica204chavez@gmail.com>
2024-10-02 17:18:47 +0800

    a

--- admin/users.php
+++ admin/users.php
@@ -2,11 +2,20 @@
 include('db_connect.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User List</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+</head>
+<body>
 <div class="container-fluid">
-    <div class="row">
-         
-    </div>
-    <br>
     <div class="row">
         <div class="card col-lg-12">
             <div class="card-body">
@@ -19,7 +28,8 @@ include('db_connect.php');
                             <th class="text-center">Email</th>
                             <th class="text-center">Mobile</th>
                             <th class="text-center">Address</th>
-                         </tr>
+                            <th class="text-center">Actions</th>
+                        </tr>
                     </thead>
                     <tbody>
                         <?php
@@ -34,8 +44,9 @@ include('db_connect.php');
                             <td><?php echo $row['email'] ?></td>
                             <td><?php echo $row['mobile'] ?></td>
                             <td><?php echo $row['address'] ?></td>
-                            <td>
-                                 
+                            <td class="text-center">
+                                <button class="btn btn-sm btn-info read_user" data-id="<?php echo $row['user_id']; ?>">Read</button>
+                                <button class="btn btn-sm btn-danger delete_user" data-id="<?php echo $row['user_id']; ?>">Delete</button>
                             </td>
                         </tr>
                         <?php endwhile; ?>
@@ -46,4 +57,76 @@ include('db_connect.php');
     </div>
 </div>
 
-   
+<!-- Read User Modal -->
+<div class="modal fade" id="readUserModal" tabindex="-1" aria-labelledby="readUserModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="readUserModalLabel">User Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="userDetails">
+                <!-- User details will be loaded here -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $('.delete_user').on('click', function() {
+            var userId = $(this).data('id');
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+                        url: 'delete_user.php',
+                        type: 'POST',
+                        data: { user_id: userId },
+                        success: function(response) {
+                            Swal.fire(
+                                'Deleted!',
+                                response,
+                                'success'
+                            ).then(() => {
+                                location.reload(); // Refresh the page to see the changes
+                            });
+                        },
+                        error: function() {
+                            Swal.fire(
+                                'Error!',
+                                'Error deleting user. Please try again.',
+                                'error'
+                            );
+                        }
+                    });
+                }
+            });
+        });
+
+        $('.read_user').on('click', function() {
+            var userId = $(this).data('id');
+            $.ajax({
+                url: 'fetch_user.php',
+                type: 'POST',
+                data: { user_id: userId },
+                success: function(data) {
+                    $('#userDetails').html(data);
+                    $('#readUserModal').modal('show');
+                },
+                error: function() {
+                    alert('Error fetching user details. Please try again.');
+                }
+            });
+        });
+    });
+</script>
+</body>
+</html>
